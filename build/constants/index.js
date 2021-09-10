91272b4e11f1b888d7fc239fb983496c68a499cc
Fabien C <fabiencoutant.ctt@gmail.com>
2021-09-10 17:05:18 +0300

    prepare heroku deploiement

---
+++ build/constants/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.POOL_POLL_INTERVAL = exports.MAX_TIME_IN_QUEUE = void 0;
+//// CONSTANTS
+exports.MAX_TIME_IN_QUEUE = 20000;
+exports.POOL_POLL_INTERVAL = 1000;
+//# sourceMappingURL=index.js.map
