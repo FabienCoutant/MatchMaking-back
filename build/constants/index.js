fa78e4992ed511fe7de64965f33c5f7040abeb6d
Fabien C <fabiencoutant.ctt@gmail.com>
2021-09-10 17:33:45 +0300

    refactor package.json

--- build/constants/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.POOL_POLL_INTERVAL = exports.MAX_TIME_IN_QUEUE = void 0;
-//// CONSTANTS
-exports.MAX_TIME_IN_QUEUE = 20000;
-exports.POOL_POLL_INTERVAL = 1000;
-//# sourceMappingURL=index.js.map
